899ae2e3a019a02f712a14c4c4ba9f0ecd5e13ce
MasterPieceSVK <112313437+MasterPieceSVK@users.noreply.github.com>
2024-06-15 09:45:14 +0200

    debug

--- upload/upload.js
+++ upload/upload.js
@@ -8,17 +8,22 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 uploadRouter.post("/", upload.single("image"), (req, res) => {
-  console.log("request");
+  console.log("Request received");
+  if (!req.file) {
+    console.error("No file received");
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
   try {
     const imageBuffer = req.file.buffer;
-    console.log("buffer");
+    console.log("File buffer received");
 
     tesseract
       .recognize(imageBuffer, "eng", {
         logger: (m) => console.log(m),
       })
       .then(({ data: { text } }) => {
-        console.log("recognized");
+        console.log("Text recognition completed");
 
         const extractedGrades = extractGrades(text);
         res.json({ grades: extractedGrades });
@@ -28,7 +33,7 @@ uploadRouter.post("/", upload.single("image"), (req, res) => {
         res.status(500).send("Error processing the image");
       });
   } catch (e) {
-    console.log(e);
+    console.error("Error processing the request:", e);
     res.status(500).json({ error: "Error (Code 2)" });
   }
 });
